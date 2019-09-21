40c7bca2d5794bdddf10a2a47121519acaf14f38
apeatling <apeatling@gmail.com>
2019-09-21 15:16:43 -0700

    Start fleshing out the placeholder settings screen for entering country code and phone number.

---
+++ src/countrycodes.js
@@ -0,0 +1,7 @@
+export const countryCodes = [
+	{ value: null, label: 'Your country code…', disabled: true },
+	{ value: '1', label: '🇺🇸 +1' },
+	{ value: '1', label: '🇨🇦 +1' },
+	{ value: '44', label: '🇬🇧 +44' },
+	{ value: '27', label: '🇿🇦 +27' },
+];
--- src/index.js
+++ src/index.js
@@ -3,27 +3,70 @@
  */
 
 import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+import {
+	Button,
+	Placeholder,
+	TextControl,
+	SelectControl,
+} from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 
 import { WhatsAppIcon } from './icon.js';
+import { countryCodes } from './countrycodes.js';
 
 registerBlockType( 'apeatling/whatsapp-block', {
 	title: 'WhatsApp',
 	icon: WhatsAppIcon,
 	category: 'widgets',
 
-	attributes: {},
+	supports: {
+		align: [ 'left', 'center', 'right' ],
+	},
+
+	styles: [
+		{ name: 'default', label: __( 'Default' ), isDefault: true },
+		{ name: 'light', label: __( 'Light' ) },
+	],
+
+	attributes: {
+		countryCode: 'string',
+		phoneNumber: 'string',
+	},
 
 	edit: ( props ) => {
+		const { attributes: { countryCode, phoneNumber }, setAttributes } = props;
+
 		return (
-			<p>placeholder</p>
+			<Placeholder
+				icon={ WhatsAppIcon }
+				label="WhatsApp"
+				instructions="Enter the phone number for your WhatsApp account."
+			>
+				<form>
+					<SelectControl
+						value={ countryCode }
+						onChange={ ( value ) => setAttributes( { countryCode: value } ) }
+						options={ countryCodes }
+					/>
+					<TextControl
+						placeholder={ __( 'Your phone number…' ) }
+						onChange={ ( value ) => setAttributes( { phoneNumber: value } ) }
+						value={ phoneNumber }
+						className={ 'components-placeholder__input' }
+					/>
+					<Button isLarge type="submit">
+						{ __( 'Insert' ) }
+					</Button>
+				</form>
+			</Placeholder>
 		);
 	},
 
-	save: ( props ) => {
+	save: () => {
 		return (
 			<p>placeholder</p>
 		);
